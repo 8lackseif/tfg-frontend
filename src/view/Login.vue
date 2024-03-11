c475edf0a1c8adb3c4983dfbc58a53a7b6e16c14
8lackseif <blackseif2001@gmail.com>
2024-03-11 19:45:53 +0100

    xd

---
+++ src/view/Login.vue
@@ -0,0 +1,32 @@
+<template>
+   
+   <div>
+    <p>{{msg}}</p>
+    <input v-model="txt" placeholder="edit me" />
+    <button class="boton" v-on:click="onSubmit">submit</button>
+   </div>
+   
+</template>
+
+<script>
+export default {
+  name: 'Login_page',
+  data () {
+    return {
+      msg: 'hello world',
+      txt: ''
+    }
+},
+methods: {
+  onSubmit: function () {
+    this.$data.msg = this.$data.txt
+  }
+}
+}
+</script>
+
+<style>
+  .boton {
+    color: blue;
+  }
+</style>
