0f3eef4bd447f153ffec20a1f9213bfdf771b7de
8lackseif <blackseif2001@gmail.com>
2024-05-12 16:09:42 +0200

    tags

--- src/view/Tags.vue
+++ src/view/Tags.vue
@@ -2,14 +2,18 @@
   <div>
     <TheHeader />
     <main class="TagsPage">
-      <div class="tableOptions">
-      <MySearcher @searchInput="search" />
+      <div class="tagsOptions">
+        <MySearcher @searchInput="search" />
       </div>
       <div class="tagsContainer fromTagsPage">
         <div class="tag" v-bind:key="t.index" v-for="t in filterTags">
           <p>{{ t.name }}</p>
           <div class="cross" v-if="editable" @click="deleteTag(t.name)">&#x2715;</div>
         </div>
+      </div>
+      <div class="addTag">
+          <b-input v-model="newTag" trim />
+          <b-btn @click="addTag">add</b-btn>
       </div>
     </main>
     <TheFooter />
@@ -29,27 +33,37 @@ export default {
       tags: [],
       searchInput: '',
       editable: true,
+      newTag: '',
+
     }
   },
   async created() {
     const payload = await this.$store.dispatch('getClaims');
-        if ('guest'.localeCompare(payload.role) === 0) {
-            this.editable = false;
-        }
+    if ('guest'.localeCompare(payload.role) === 0) {
+      this.editable = false;
+    }
 
     this.tags = await this.$store.dispatch('getTags');
   },
   methods: {
-    search: async function (searchInput){
+    search: async function (searchInput) {
       this.searchInput = searchInput;
     },
-    deleteTag: async function(tagname) {
+    deleteTag: async function (tagname) {
       const query = {
         token: await this.$store.dispatch('getToken'),
         name: tagname
       }
       this.$store.dispatch('deleteTag', query);
-      this.tags = this.tags.filter(t=> t.name != tagname);
+      this.tags = this.tags.filter(t => t.name != tagname);
+    },
+    addTag: async function() {
+      const query = {
+        token: await this.$store.dispatch('getToken'),
+        name: this.newTag
+      }
+      await this.$store.dispatch('addTag', query);
+      this.tags = await this.$store.dispatch('getTags');
     }
   },
   computed: {
@@ -75,5 +89,21 @@ export default {
 
 .fromTagsPage {
   margin: 0vh 1.4vw;
+}
+
+.tagsOptions {
+  display: flex;
+  height: 5vh;
+  margin: 1vh;
+  gap: 1vw;
+  width: 25vw;
+}
+
+.addTag {
+  display: flex;
+  height: 5vh;
+  margin: 2vh;
+  width: 20vw;
+  gap: 0.5vw;
 }
 </style>
